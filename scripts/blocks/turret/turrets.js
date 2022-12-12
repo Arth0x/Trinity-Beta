04d7ac92948e6a8727e323a51ae034589cc44725
Arthox <61596950+Arth0x@users.noreply.github.com>
2022-12-12 10:14:30 +0100

    x

---
+++ scripts/blocks/turret/turrets.js
@@ -0,0 +1,7 @@
+// Lobber
+require("mechanical-warfare/blocks/turret/lobber")
+
+
+
+// M.A.R.S
+require("mechanical-warfare/blocks/turret/mars");
--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,25 @@
+//blocks
+require("mindustrinity/blocks/production")
+require("mindustrinity/blocks/drills")
+require("mindustrinity/blocks/power")
+require("mindustrinity/blocks/turret/turrets")
+require("mindustrinity/blocks/defense")
+require("mindustrinity/blocks/effect/element-aura")
+require("mindustrinity/blocks/effect/slag-bomb")
+require("mindustrinity/blocks/effect/spotlight")
+require("mindustrinity/blocks/effect/core-sword")
+
+//units
+require("mindustrinity/units/ground-unit")
+require("mindustrinity/units/hover-unit")
+require("mindustrinity/units/flying-unit")
+require("mindustrinity/units/copter-unit")
+require("mindustrinity/units/worm-unit")
+require("mindustrinity/units/tank-unit")
+
+//mechs
+require("mindustrinity/mechs/ship")
+require("mindustrinity/mechs/mech")
 
 require("copters")
 require('Planet')
