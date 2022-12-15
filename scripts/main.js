1306cca74ebd71455ebbce3410d6649d80fcf7de
Arthox <61596950+Arth0x@users.noreply.github.com>
2022-12-15 10:30:41 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -7,14 +7,13 @@ require('Planet')
 Events.on(UnitDestroyEvent, event => {
     // display toast on top of screen when the unit was a player
     if(event.unit.isPlayer()){
-        Vars.ui.hudfrag.showToast("big noob");
+        const myDialog = new BaseDialog("Dialog Title");
+        // Add "go back" button
+        myDialog.addCloseButton();
+        // Add text to the main content
+        myDialog.cont.add("Goodbye.");
+        // Show dialog
+        myDialog.show();
     }
 })
 
-const myDialog = new BaseDialog("Dialog Title");
-// Add "go back" button
-myDialog.addCloseButton();
-// Add text to the main content
-myDialog.cont.add("Goodbye.");
-// Show dialog
-myDialog.show();
